4f0953b3ed62b874e4c0243bffd4c85ce2b2c54b
Damacious <damaciouswhite@yahoo.com>
2018-10-22 06:44:30 -0400

    Lab 10 Classes

---
+++ Lab10/Lab10/Circle.cs
@@ -0,0 +1,32 @@
+﻿using System;
+namespace Lab10
+{
+    public class Circle
+    {
+        public Circle(double radius){}
+
+        public double CalculateCircumference()
+        {
+            double Circumference = (2 * System.Math.PI * Radius);
+
+        }
+
+        public string CalculateFormattedCircumference()
+        {}
+
+        public double CalculateArea()
+        {
+            Area = (Length * Width);
+        }
+
+        public string CalculateFormattedArea()
+        {}
+
+        private string FormatNumber(double x){}
+
+
+
+
+
+    }
+}
---
+++ Lab10/Lab10/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Lab10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CalculateCircumference();
+            Console.WriteLine("Lab 10");
+            Console.WriteLine("Welcome to Circle Tester");
+            Console.WriteLine("Enter radius :");
+            double Radius = double.Parse(Console.ReadLine());
+
+            NewCircumference();
+            {
+                Circle cl = new Circle();
+                Console.WriteLine("Circumference : " + cl.CalculateCircumference());
+            }
+           
+        }
+    }
+}
+
